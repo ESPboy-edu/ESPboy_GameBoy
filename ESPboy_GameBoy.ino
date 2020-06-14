4dac9d6bc9454ce4b434bfa7670353803216ded7
ESPboy <ESPboy.edu@gmail.com>
2020-06-14 09:04:20 +0300

    minor fixes

--- ESPboy_GameBoy.ino
+++ ESPboy_GameBoy.ino
@@ -1,3 +1,19 @@
+/*
+# ESPboy_GameBoy
+GameBoy emulator for ESPboy. 
+
+You are able to try Nintendo retro games like SuperMario, Zelda, Pokemon, etc! 
+Pressing both side buttons, you can adjust view port (original GB screen resolution is 160х144 but it's only 128x128 window visible on ESPboy display).
+
+by RomanS for ESPboy project
+https://hackaday.io/project/164830-espboy-games-iot-stem-for-education-fun
+
+Peanut-GB core is used
+https://github.com/deltabeard/Peanut-GB
+
+MIT license
+*/
+
 #include "Arduino.h"
 #include <Adafruit_MCP23017.h>
 #include <Adafruit_MCP4725.h>
@@ -39,6 +55,7 @@ uint8_t OFFSET_X=16, OFFSET_Y=8;
 
 static struct gb_s gb;
 enum gb_init_error_e ret;
+
 
 uint8_t inline getKeys() { return (~mcp.readGPIOAB() & 255); }
 
